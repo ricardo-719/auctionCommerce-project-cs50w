a745a3dea667ff50749f5c5ac7f6b2a2947bf708
ricardo-719 <teacherrickyricardo@gmail.com>
2022-12-29 17:40:59 +0700

    Update with initial models

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# Remember that each time you change anything in auctions/models.py, 
+# you’ll need to first run python manage.py makemigrations and then 
+# python manage.py migrate to migrate those changes to your database.
 class User(AbstractUser):
     pass
+
+class AuctionListings(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.CharField(max_length=64)
+    initialBid = models.IntegerField()
+    date = models.IntegerField()
+
+class Bids(models.Model):
+    user = models.CharField(max_length=64)
+    bid = models.IntegerField()
+
+class Comments(models.Model):
+    user = models.CharField(max_length=64)
+    comment = models.CharField(max_length=280)
