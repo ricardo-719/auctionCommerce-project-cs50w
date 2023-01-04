8c1fc0d96d736b9392c3d6d9423f8b3621c83c5c
ricardo-719 <teacherrickyricardo@gmail.com>
2023-01-04 19:35:08 +0700

    Update listings' model

--- auctions/models.py
+++ auctions/models.py
@@ -6,17 +6,38 @@ from django.db import models
 # python manage.py migrate to migrate those changes to your database.
 class User(AbstractUser):
     pass
+    #id = models.IntegerField()
+    #username = models.CharField(max_length=64)
+    #name = models.CharField(max_length=100)
+    #password should be a hash
+    #passwordHash = models.CharField()
 
 class AuctionListings(models.Model):
-    name = models.CharField(max_length=100)
-    user = models.CharField(max_length=64)
+    LISTING_CATEGORIES = (
+        ('Home'),
+        ('Fashion'),
+        ('Toys'),
+        ('Electronics'),
+        ('Baby'),
+        ('Automotive'),
+        ('Other'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    itemTitle = models.CharField(max_length=80)
+    itemDescription = models.CharField(max_length=450)
+    listingImg = models.CharField(max_length=100)
     initialBid = models.IntegerField()
-    date = models.IntegerField()
+    category = models.CharField(max_length=15, choices=LISTING_CATEGORIES)
+    isActive = models.BooleanField(default=True)
+    date = models.DateField()
 
-class Bids(models.Model):
-    user = models.CharField(max_length=64)
-    bid = models.IntegerField()
+#class Bids(models.Model):
+    #user = models.CharField(max_length=64)
+    #bid = models.IntegerField()
 
-class Comments(models.Model):
-    user = models.CharField(max_length=64)
-    comment = models.CharField(max_length=280)
+#class Comments(models.Model):
+    #user = models.CharField(max_length=64)
+    #comment = models.CharField(max_length=280)
+
+    #def __str__(self):
+        #return f"{ self.user } commented { self.comment }"
