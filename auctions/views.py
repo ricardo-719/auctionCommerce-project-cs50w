a08b5af2c6048944eb743b9c404cf51058f2ecce
ricardo-719 <teacherrickyricardo@gmail.com>
2023-02-09 19:16:00 +0700

    Add style to index, log-in & resgister page

--- auctions/views.py
+++ auctions/views.py
@@ -36,7 +36,7 @@ class AuctionListingsForm(ModelForm):
                 'placeholder': 'USD'
             }),
             'category': Select(attrs={
-                'class': "form-control",
+                'class': "form-select px-2 py-1.5 text-base font-normal text-gray-700 border border-solid border-gray-300 rounded transition ease-in-out m-1 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none",
                 'style': 'max-width: 150px',
                 'id': 'categoryFormId'
             }),
